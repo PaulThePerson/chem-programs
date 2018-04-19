0e2ebcbc7bc5e922629617a03945c4b04df07d12
PaulThePerson <pauldm1188@gmail.com>
2018-04-19 08:26:49 -0700

    Limiting
    Adds a limiting reagent function

--- chem.js
+++ chem.js
@@ -2,7 +2,7 @@ var atoms = ['H','He','Li','Be','B','C','N','O','F','Ne','Na','Mg','Al','Si','P'
 var weights = [1.008,4.002,6.941,9.012,10.814,12.011,14.007,15.999,18.998,20.179,22.989,24.305,26.981,28.085,30.973,32.060,35.450,39.948,39.098,40.078,44.955,47.867,50.941,51.996,54.938,55.845,58.933,58.693,63.546,65.380,69.723,72.630,74.921,78.971,79.904,83.798,85.468,87.620,88.905,91.224,92.906,95.950,98,101.070,102.906,106.420,107.868,112.414,114.818,118.710,121.760,127.600,126.90447,131.293,132.90545196,137.327,138.90547,140.116,140.90766,144.242,145,150.36,151.964,157.25,158.92535,162.500,164.93033,167.259,168.93422,173.045,174.9668,178.49,180.94788,183.84,186.207,190.23,192.217,195.084,196.966569,200.592,204.38,207.2,208.98040,209,210,222,223,226,227,232.0377,231.03588,238.02891,237,244,243,247,247,251,252,257,258,259,266,267,268,269,270,277,278,281,282,285,286,289,290,293,294,294];
 var regex1 = /[A-Z][^A-Z]*/g;
 var last = "CH4+2O2=CO2+2H2O";
-
+var boxValues=[];
 
 function molecularWeight(molecule) {
 	var tWeight = 0;
@@ -73,6 +73,18 @@ function Tester(num){
 	});
 }
 
+function limitingReagent(matrix,weights,places){
+	if((matrix[2][places[0]]/weights[0])/(matrix[2][places[1]]/weights[1])>0.9&&(matrix[2][places[0]]/weights[0])/(matrix[2][places[1]]/weights[1])<1.1){
+		return "balanced to a reasonable amount";
+	}
+	if(matrix[places[0]]/weights[0]>matrix[places[1]]/weights[1]){
+		return "reactant "+matrix[0][places[0]]+" is the limiting reagent. You need "+solveRatio(matrix[2],weights[1],places[1])[places[0]];
+	}
+	else{
+		return "reactant "+matrix[0][places[1]]+" is the limiting reagent. You need "+solveRatio(matrix[2],weights[0],places[0])[places[1]];
+	}
+}
+
 function makeTable(matrix) {
 	var thing = document.getElementById("box");
 	while (thing.children.length > 1) {
@@ -96,7 +108,8 @@ function makeTable(matrix) {
 		box.appendChild(b);
 
 		var c = document.createElement("p");
-		c.innerHTML = parseInt(matrix[2][i]*1000)/1000;
+		c.innerHTML = parseInt(matrix[2][i]*1000\
+			000;
 		box.appendChild(c);
 
 		var d = document.createElement("input");
