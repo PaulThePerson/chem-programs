cb084f0aaa605d68a2c3dcffb33605396970b387
PaulThePerson <pauldm1188@gmail.com>
2018-09-11 16:12:39 -0700

    Build up architecture for ionic formuli

--- test.js
+++ test.js
@@ -1,144 +1,45 @@
-function invertMatrix(M){
-  //I didnt make this code. I tried. As seen above. I failed. SO... you get this code
-  //enjoy.
-  //maybe I can try to make my own later.
-  // source at http://blog.acipo.com/matrix-inversion-in-javascript/
-  //just this function
-  //i did the rest
-  //just to be clear
-  if(M.length !== M[0].length){return;}
-  var i=0, ii=0, j=0, dim=M.length, e=0, t=0;
-  var I = [], C = [];
-  for(i=0; i<dim; i+=1){
-    I[I.length]=[];
-    C[C.length]=[];
-    for(j=0; j<dim; j+=1){
-      if(i==j){ I[i][j] = 1; }
-      else{ I[i][j] = 0; }
-      C[i][j] = M[i][j];
-    }
+var atoms = ['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og'];
+var weights = [1.008, 4.002, 6.941, 9.012, 10.814, 12.011, 14.007, 15.999, 18.998, 20.179, 22.989, 24.305, 26.981, 28.085, 30.973, 32.060, 35.450, 39.948, 39.098, 40.078, 44.955, 47.867, 50.941, 51.996, 54.938, 55.845, 58.933, 58.693, 63.546, 65.380, 69.723, 72.630, 74.921, 78.971, 79.904, 83.798, 85.468, 87.620, 88.905, 91.224, 92.906, 95.950, 98, 101.070, 102.906, 106.420, 107.868, 112.414, 114.818, 118.710, 121.760, 127.600, 126.90447, 131.293, 132.90545196, 137.327, 138.90547, 140.116, 140.90766, 144.242, 145, 150.36, 151.964, 157.25, 158.92535, 162.500, 164.93033, 167.259, 168.93422, 173.045, 174.9668, 178.49, 180.94788, 183.84, 186.207, 190.23, 192.217, 195.084, 196.966569, 200.592, 204.38, 207.2, 208.98040, 209, 210, 222, 223, 226, 227, 232.0377, 231.03588, 238.02891, 237, 244, 243, 247, 247, 251, 252, 257, 258, 259, 266, 267, 268, 269, 270, 277, 278, 281, 282, 285, 286, 289, 290, 293, 294, 294];
+
+
+function basicMolecularWeight(molecule) {
+  var tWeight = 0;
+  if (molecule.match(regex1) === null) {
+    return "Invalid";
   }
-  for(i=0; i<dim; i+=1){
-    e = C[i][i];
-    if(e==0){
-      for(ii=i+1; ii<dim; ii+=1){
-        if(C[ii][i] != 0){
-          for(j=0; j<dim; j++){
-            e = C[i][j];
-            C[i][j] = C[ii][j];
-            C[ii][j] = e;
-            e = I[i][j];
-            I[i][j] = I[ii][j];
-            I[ii][j] = e;
-          }
-          break;
-        }
-      }
-      e = C[i][i];
-      if(e==0){return}
+  molecule.match(regex1).forEach(function(mPart) {
+    if (!(/\d+$/.test(mPart))) {
+      mPart += "1";
     }
-    for(j=0; j<dim; j++){
-      C[i][j] = C[i][j]/e;
-      I[i][j] = I[i][j]/e;
-    }
-    for(ii=0; ii<dim; ii++){
-      if(ii==i){continue;}
-      e = C[ii][i];
-      for(j=0; j<dim; j++){
-        C[ii][j] -= e*C[i][j];
-        I[ii][j] -= e*I[i][j];
-      }
-    }
-  }
-    return I;
+    tWeight += parseFloat(mPart.match(/\d+$/)) * weights[atoms.indexOf(mPart.match(/[A-Z][a-z]*/)[0])];
+  });
+  return tWeight;
 }
 
-function fixReturn(thing){
-  var sum=0;
-  if(thing===null){
+function fixReturn(thing) {
+  var sum = 0;
+  if (thing === null) {
     return 0;
   }
-  thing.forEach(function(thing2){
-    console.log(thing2,thing);
-    if(thing2===""){
-      sum+=1;
+  thing.forEach(function(thing2) {
+    //console.log(thing2, thing);
+    if (thing2 === "") {
+      sum += 1;
+    } else {
+      sum += parseInt(thing2);
     }
-    else{
-      sum+=parseInt(thing2);
-    }
-  })
+  });
+  //console.log(sum);
   return sum;
 }
 
-function mEquation(equation){
-  try{
-    var elems=[];
-    var molcs=equation.match(/[A-Z][^+=]*/gm);
-    equation.match(/[A-Z][^+=]*/gm).forEach(function(molecule){
-      console.log(molecule);
-      molecule.match(/[A-Z][a-z]?/gm).forEach(function(element){
-        if(elems.indexOf(element)<0){
-          elems.push(element);
-    }})})
-    console.log(elems,molcs);
-    var matrixP=[];
-    elems.forEach(function(element){
-      matrixP.push([]);
-      molcs.forEach(function(molecule){
-        console.log("molecule = " + molecule+", tested element = "+element);
-        console.log("match = " + molecule.match(new RegExp("(?<="+element+")[0-9]*","gm")));
-        matrixP[matrixP.length-1].push(fixReturn(molecule.match(new RegExp("(?<="+element+")[0-9]*","gm"))));
-      })
-    })
-    console.log(matrixP);
-    while(matrixP[0].length>matrixP.length){
-      matrixP.push([]);
-      for(var i=0; i<matrixP[0].length; i++){
-        if(i===matrixP.length-1){
-          matrixP[matrixP.length-1].push(1);
-        }
-        else{
-          matrixP[matrixP.length-1].push(0);
-        }
-      }
-    }
-    var ratio=[];
-    var matrix=invertMatrix(matrixP);
-    for(var i=0;i<matrix.length;i++){
-      ratio.push(matrix[i][matrix.length-1]);
-    }
-    console.log(ratio);
-    var quitValue=3000;
-    var multiplier=0;
-    while(quitValue!==0){
-      multiplier++;
-      quitValue++;
-      var bad=0;
-      ratio.forEach(function(numb){
-        if(!(Math.round(numb*1000*multiplier)%1000===0)){
-          bad=1;
-        }
-      })
-      if(bad===0){break;}
-    }
-    console.log(multiplier);
-    answer="";
-    for(var i=0;i<ratio.length;i++){
-      ratio[i]=ratio[i]*multiplier;
-      answer+=Math.abs(ratio[i])+molcs[i];
-      if(ratio.length-1>i){
-        if(ratio[i+1]*ratio[i]>0){
-          answer+="+";
-        }
-        else{
-          answer+="="
-        }
-      }
-    }
-    console.log(answer);
-    return [answer,0];
-  }
-  catch(TypeError){
-    return [equation,1];
-  }
+function parWeight(molecule){
+  var ioP=molecule.match(/[\(][^\)]+[\)][0-9]*/gm);
+  var tWeight=0;
+  var removedMolecule=molecule;
+  ioP.forEach(function(thingy) {
+    tWeight += parseFloat(basicMolecularWeight(thingy.match(/[A-Z][^\)]+/))*parseFloat(fixReturn(thingy.match(/[\)][0-9]*/).match(/[0-9]*/)))
+    removedMolecule=removedMolecule.replace("thingy", "")
+  });
+  tWeight+=basicMolecularWeight(removedMolecule)
 }
